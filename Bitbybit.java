58d0dc2af1252e4577442d29ee9f6f9f2da2e4e5
mariarenasarb <148344496+mariarenasarb@users.noreply.github.com>
2023-12-01 11:42:39 +0200

    new method

--- Bitbybit.java
+++ Bitbybit.java
@@ -1,11 +1,14 @@
 public class Bitbybit{
 	public static void main(String[] args){
-	 System.out.println("Write your names");
-	 System.out.println("President of your heart was here");
-	 System.out.println("Kerase tzi mia koupa");
-	 System.out.println("Evaki kouklaki");
-	 System.out.println("ligi sovarotita parakalo...");
-         System.out.println("OMG EPITELOUS TO KATALAVA");
-	 System.out.println("test by mary");
+	 	System.out.println("Write your names");
+	 	System.out.println("President of your heart was here");
+	 	System.out.println("Kerase tzi mia koupa");
+	 	System.out.println("Evaki kouklaki");
+	 	System.out.println("ligi sovarotita parakalo...");
+    	System.out.println("OMG EPITELOUS TO KATALAVA");
+	 	System.out.println("test by mary");
+	}
+	public static int result() {
+		return 2;
 	}
 }
