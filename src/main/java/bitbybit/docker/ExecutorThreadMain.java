6bf5eb0e745e637dffada98a35c1f1d6ba3994de
mariarenasarb <148344496+mariarenasarb@users.noreply.github.com>
2023-12-02 17:30:43 +0200

    Add files via upload

---
+++ src/main/java/bitbybit/docker/ExecutorThreadMain.java
@@ -0,0 +1,38 @@
+package bitbybit.docker;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+public class ExecutorThreadMain {
+    public static void main(String[] args) {
+        String containerID = "your_container_id_here";
+
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
+
+        ExecutorThread executorThread = new ExecutorThread(containerID, dockerClient);
+        executorThread.startThread();
+
+        // Usage examples of the methods
+        executorThread.pauseThread(); // Pausing the thread
+
+        // Simulating some delay...
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        executorThread.resumeThread(); // Resuming the thread
+
+        // Simulating some more processing...
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        executorThread.stopThread(); // Stopping the thread
+    }
+}
